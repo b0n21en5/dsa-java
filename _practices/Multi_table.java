e1ab56a344eb089e73f45a2a8aba909996c79420
Bikash Nath <splittergame91@gmail.com>
2022-10-16 15:56:59 +0530

    20 - Multiplication_Table

---
+++ _practices/Multi_table.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Multi_table {
+	
+	public static void main(String[] args) {
+		Scanner sn = new Scanner(System.in);
+        int n = sn.nextInt();
+        int i = 1;
+        while (i<=10){
+            System.out.println(i*n);
+            i++;
+        }
+
+	}
+}
