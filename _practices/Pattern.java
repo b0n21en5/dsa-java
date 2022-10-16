1b6939ba15b08dcab1a7914bb6bba8fb44eff671
Bikash Nath <splittergame91@gmail.com>
2022-10-16 23:52:59 +0530

    24 - Pattern_printing

--- _practices/Pattern.java
+++ _practices/Pattern.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.*;
 public class Pattern {
     public static void main(String[] args) {
 
@@ -6,19 +6,99 @@ public class Pattern {
         // i.e. 3== ***
                 //  ***
                 //  ***
-
                 
-        Scanner s = new Scanner(System.in);
-        int i=1, n = s.nextInt();
-        while (i<=n){
-            int j =1;
-            while (j<=n){
-                System.out.print("*");
-                j++;
-            }
-            System.out.println();
-            i++;
-        }
+        // Scanner s = new Scanner(System.in);
+        // int i=1, n = s.nextInt();
+        // while (i<=n){
+        //     int j =1;
+        //     while (j<=n){
+        //         System.out.print("*");
+        //         j++;
+        //     }
+        //     System.out.println();
+        //     i++;
+        // }
+
+
+
+
+        // This program will print star for n rows and n columns
+        // i.e. 3== *
+                //  **
+                //  ***
+                
+        // Scanner s = new Scanner(System.in);
+        // int i=1, n = s.nextInt();
+        // while (i<=n){
+        //     int j =1;
+        //     while (j<=i){
+        //         System.out.print("*");
+        //         j++;
+        //     }
+        //     System.out.println();
+        //     i++;
+        // }
+
+
+
+
+
+        // This program will print star for n rows and n columns
+        // i.e. 3==   *
+                //   **
+                //  ***
+
+        // Scanner s = new Scanner(System.in);
+        // int i=1, n = s.nextInt();
+        
+        // while (i<=n){
+        //     int k=i;
+        //     while (k<n){
+        //         System.out.print(" ");
+        //         k++;
+        //     }
+        //     int j =1;
+        //     while (j<=i){
+        //         System.out.print("*");
+        //         j++;
+        //     }
+        //     System.out.println();
+        //     i++;
+        // }
+
+
+
+
+
+
+
+        // This program will print star for n rows and n columns
+        // i.e. 3==   ***
+                //    **
+                //    *
+
+        // Scanner s = new Scanner(System.in);
+        // int i=1, n = s.nextInt();
+        // while (i<=n){
+        //     int j =1;
+        //     int v = n+1-i;
+        //     while (j<=v){
+        //         System.out.print("*");
+        //         j++;
+        //     }
+        //     System.out.println();
+        //     i++;
+        // }
+
+
+
+
+
+
+
+        
+
+
 
 
 
@@ -26,7 +106,6 @@ public class Pattern {
         // If n is 3 This programe will print 111
                 //                            222
                 //                            333
-
 
         // Scanner s = new Scanner(System.in);
         // int i=1, n = s.nextInt();
@@ -41,10 +120,12 @@ public class Pattern {
         // }
 
 
+
+
+
         // If n is 3 This programe will print 123
                 //                            123
                 //                            123
-
 
         // Scanner s = new Scanner(System.in);
         // int i=1, n = s.nextInt();
@@ -57,6 +138,8 @@ public class Pattern {
         //     System.out.println();
         //     i++;
         // }
+
+
 
 
 
@@ -78,9 +161,12 @@ public class Pattern {
 
 
 
+
+
         // If n is 3 This programe will print 333
                 //                            333
                 //                            333
+
         // Scanner s = new Scanner(System.in);
         // int i=1, n = s.nextInt();
         // while (i<=n){
@@ -93,6 +179,359 @@ public class Pattern {
         //     i++;
         // }
             
+
+
+
+
+        // If n is 4 This code will print   1
+        //                                  12
+        //                                  123
+        //                                  1234
+
+
+        // Scanner s = new Scanner(System.in);
+        // int i=1, n = s.nextInt();
+        // while (i<=n){
+        //     int j =1;
+        //     while (j<=i){
+        //         System.out.print(j);
+        //         j++;
+        //     }
+        //     System.out.println();
+        //     i++;
+        // }
+
+
+
+
+
+        // If n is 4 This code will print   1
+        //                                  23
+        //                                  456
+        //                                  78910
+
+
+        // Scanner s = new Scanner(System.in);
+        // int p=1, i=1, n = s.nextInt();
+        // while (i<=n){
+        //     int j =1;
+        //     while (j<=i){
+        //         System.out.print(p);
+        //         j++;
+        //         p++;
+        //     }
+        //     System.out.println();
+        //     i++;
+        // }
+
+
+
+
+
+
+
+        // If n is 4 This code will print   1
+        //                                  23
+        //                                  345
+        //                                  5678
+
+
+        // Scanner s = new Scanner(System.in);
+        // int p, i=1, n = s.nextInt();
+        // while (i<=n){
+        //     p=i;
+        //     int j =1;
+        //     while (j<=i){
+        //         System.out.print(p);
+        //         j++;
+        //         p++;
+        //     }
+        //     System.out.println();
+        //     i++;
+        // }
         
+
+
+
+
+
+        // If n is 4 This code will print   1
+        //                                  21
+        //                                  321
+        //                                  4321
+       
+	// Scanner s = new Scanner(System.in);
+        // int i=1, n = s.nextInt();
+        // while (i<=n){
+        //     int j =i;
+        //     while (j>0){
+        //         System.out.print(j);
+        //         --j;
+        //     }
+        //     System.out.println();
+        //     i++;
+        // }
+
+
+
+
+
+
+        // This program will print star for n rows and n columns
+        // i.e. 3==   1
+                //   121
+                //  12321
+
+        // Scanner s = new Scanner(System.in);
+        // int i=1, n = s.nextInt();
+        
+        // while (i<=n){
+        //     int k=i;
+        //     while (k<n){
+        //         System.out.print(" ");
+        //         k++;
+        //     }
+        //     int j =1;
+        //     while (j<=i){
+        //         System.out.print(j);
+        //         j++;
+        //     }
+        //     int num = i-1;
+        //     while (num>0){
+        //         System.out.print(num);
+        //         num--;
+        //     }
+        //     System.out.println();
+        //     i++;
+        // }
+
+
+
+
+
+
+        // This program will print star for n rows and n columns
+        // i.e. 3==   1
+                //   121
+                //  12321
+
+                // Scanner s = new Scanner(System.in);
+                // int i=1, n = s.nextInt();
+                
+                // while (i<=n){
+                //     int k=i;
+                //     while (k<n){
+                //         System.out.print(" ");
+                //         k++;
+                //     }
+                //     int p=i, j =1;
+                //     while (j<=i){
+                //         System.out.print(p);
+                //         j++;
+                //         p++;
+                //     }
+                //     int num = p-2;
+                //     while (num>=i){
+                //         System.out.print(num);
+                //         num--;
+                //     }
+                //     System.out.println();
+                //     i++;
+                // }  
+                
+                
+
+
+
+
+
+                // This program will print star for n rows and n columns
+        // i.e. 3==   1
+                //   121
+                //  12321
+
+                // Scanner s = new Scanner(System.in);
+                // int i=1, n = s.nextInt();
+                
+                // while (i<=n){
+                //     int k=i;
+                //     while (k<n-2){
+                //         System.out.print(" ");
+                //         k++;
+                //     }
+                //     int p=i, j =1;
+                //     while (j<=i){
+                //         System.out.print("*");
+                //         j++;
+                //         p++;
+                //     }
+                //     int num = p-2;
+                //     while (num>=i){
+                //         System.out.print("*");
+                //         num--;
+                //     }
+                //     System.out.println();
+                //     i++;
+                // } 
+
+
+
+
+
+        // If n is 4 This code will print   ABCD
+        //                                  ABCD
+        //                                  ABCD
+        //                                  ABCD
+
+        // Scanner s = new Scanner(System.in);
+        // int i=1, n = s.nextInt();
+        // while (i<=n){
+        //     int j =1;
+        //     char c ='A';
+        //     while (j<=n){
+        //         System.out.print(c);
+        //         j++;
+        //         c++;
+        //     }
+        //     System.out.println();
+        //     i++;
+        // }
+
+
+
+
+		
+        // If n is 4 This code will print   A
+        //                                  BB
+        //                                  CCC
+        //                                  DDDD
+
+        // Scanner s = new Scanner(System.in);
+        // int i=1, n = s.nextInt();
+        // char c='A';
+        // while (i<=n){
+        //     int j =1;
+        //     while (j<=i){
+        //         System.out.print(c);
+        //         j++;
+        //     }
+        //     System.out.println();
+        //     c++;
+        //     i++;
+        // }
+
+
+
+
+
+        // If n is 4 This code will print   ABCD
+        //                                  BCDE
+        //                                  CDEF
+        //                                  DEFG
+
+        // Scanner s = new Scanner(System.in);
+        // int p, i=1, n = s.nextInt();
+        
+        // while (i<=n){
+        //     char c=(char) ('A' +i-1);
+        //     int j =1;
+        //     while (j<=n){
+        //         System.out.print(c);
+        //         j++;
+        //         c++;
+        //     }
+        //     System.out.println();
+        //     i++;
+        // }
+
+
+
+
+
+        // If n is 4 This code will print   A
+        //                                  BC
+        //                                  CDE
+        //                                  DEFG
+
+        // Scanner s = new Scanner(System.in);
+        // int p, i=1, n = s.nextInt();
+        
+        // while (i<=n){
+        //     char c=(char) ('A' +i-1);
+        //     int j =1;
+        //     while (j<=i){
+        //         System.out.print(c);
+        //         j++;
+        //         c++;
+        //     }
+        //     System.out.println();
+        //     i++;
+        // }
+
+
+
+
+
+        // If n is 4 This code will print   D
+        //                                  CD
+        //                                  BCD
+        //                                  ABCD
+
+        // Scanner s = new Scanner(System.in);
+        // int p, i=1, n = s.nextInt();
+        
+        // while (i<=n){
+        //     char c=(char) ('A' +n-i );
+        //     int j =1;
+        //     while (j<=i){
+        //         System.out.print(c);
+        //         j++;
+        //         c++;
+        //     }
+        //     System.out.println();
+        //     i++;
+        // }
+
+
+
+
+
+        Scanner s = new Scanner(System.in);
+        int i=1, r=2, n1,n2, n = s.nextInt();
+        n1 = (n+1)/2;
+        while (i<=n1){
+                int sp = n1-i;
+                int j =1;
+                while (j<=sp){
+                        System.out.print(" ");
+                        j++;
+                }
+                int k=1,st = (2*i) - 1;
+                while (k<=st){
+                        System.out.print("*");
+                        k++;
+                }
+                System.out.println();
+                i++;
+        }
+        n2 = ((n+1)/2)-1;
+        while (r>=1){
+                int t=1,sp = n2-r+1;
+                while (t<=sp){
+                        System.out.print(" ");
+                        t++;
+                }
+                int ki=1,star = (2*r) - 1;
+                while (ki<=star){
+                        System.out.print("*");
+                        ki++;
+                }
+                
+                System.out.println();
+                
+                r--;
+        }
+
+
     }
 }
