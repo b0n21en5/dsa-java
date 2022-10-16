7af6be142079a3dc7a50d539942408cd81fcc906
Bikash Nath <splittergame91@gmail.com>
2022-10-16 15:39:05 +0530

    19 - Salary_counted

---
+++ _practices/Salary.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Salary {
+    public static void main (String[] args) {
+        Scanner sn = new Scanner(System.in);
+        float hra,da,pf,all;
+        int ts = sn.nextInt();
+        String st = sn.next().toUpperCase();
+        char g = st.charAt(0);
+        hra = (ts * .2f);
+        da =  (ts * .5f);
+        pf =  (ts * .11f);
+        if (g=='A'){
+            all = 1700;
+        }else if (g=='B'){
+            all = 1500;
+        }else{
+            all = 1300;
+        }
+        System.out.println(Math.round(ts+hra+da+all-pf));
+    }
+}
