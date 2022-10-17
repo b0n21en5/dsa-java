486e5840c10af65cc0071638c02b29cb3d34cc42
Bikash Nath <splittergame91@gmail.com>
2022-10-18 04:40:55 +0530

    33 - Integral_Square_Root

---
+++ _practices/INT_SQRT.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class INT_SQRT {
+    public static void main(String[] args) {
+        Scanner s= new Scanner(System.in);
+        double n=s.nextDouble();
+        int sqrt = (int) (Math.sqrt(n));
+        System.out.println(sqrt);
+
+    }
+}
