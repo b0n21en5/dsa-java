c8a34e73f95d6565c32c797bd35593f1236d39e7
Bikash Nath <splittergame91@gmail.com>
2022-10-25 22:46:33 +0530

    57 - isPalindrome_String

--- _practices/isPalindrome_String.java
+++ _practices/isPalindrome_String.java
@@ -18,11 +18,24 @@ public class isPalindrome_String {
         return res;
     }
 
+
+    public static boolean isPali_two(String str){
+        String reverse = "";
+        for (int i=str.length()-1;i>=0;i--){
+            reverse+=str.charAt(i);
+        }
+        if (str.equals(reverse)){return true;}
+        else{return false;}
+    }
+
+
+
 	public static void main(String[] args) {
 		Scanner sn=new Scanner(System.in);
         String str = sn.next();
         str = str.toLowerCase();
         System.out.println(isPalindrome(str));
+        System.out.println(isPali_two(str));
 
         sn.close();
 	}
