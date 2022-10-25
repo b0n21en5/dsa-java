eba15936d154c369a778ca7c9fa80ca0c1fe7187
Bikash Nath <splittergame91@gmail.com>
2022-10-25 23:55:06 +0530

    58 - String_operations

---
+++ _practices/String.java
@@ -0,0 +1,47 @@
+class String_operations{
+    public static void main(String[] args) {
+        // String a ="abcd";
+        // String b="abcda";
+        // System.out.println(a.compareTo(b));
+
+        // String a="coding",b="ninjas";
+        // if(a.contains("ing"))
+        // {
+        //     a+=b;
+        // }
+        // else
+        // {
+        //     b+="ing";
+        // }
+        // System.out.print(b+a);
+        // }
+
+
+        // String a="coding";
+        // System.out.println(a.substring(2));
+        // for(int i=2;i<4;i++)
+        // {
+        //     System.out.print(a.substring(i));
+        // }
+
+
+        // String a="coding";
+        // for(int i=2;i<5;i++)
+        // {
+        //     System.out.print(a.substring(i-2,i+1));
+        // }
+
+
+
+        // String str1="abc";
+        // String str2=new String("abc");
+        // System.out.println(str1.equals(str2));
+
+
+
+        String str1="abc";
+        String str2="";
+        System.out.println(str1.contains(str2));
+    
+}
+}
