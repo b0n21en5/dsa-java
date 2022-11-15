e102f69a44a3b6447d585c7efcb436289024dc34
Bikash Nath <splittergame91@gmail.com>
2022-11-15 16:03:04 +0530

    6 - alternate to IF-ELSE

---
+++ _basic/Ternary_Operator.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Ternary_Operator {
+
+    // ternary operator is alternate to 
+    // if-else conditional statement
+    // syntax of ternary is follows:
+    // condition ? ifConditionTrue : ifConditionFalse;
+    
+
+    public static void main (String[] args) throws java.lang.Exception
+	{
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int t = sc.nextInt();
+		
+		while (t>0){
+		    int x = sc.nextInt();
+
+            //below line of code is ternary operator
+		    String res = x >= 2000 ? "YES" : "NO"; //storing value to a variable condition wise
+
+            //printing the result
+		    System.out.println(res);
+		    t--;
+		}
+	}
+}
