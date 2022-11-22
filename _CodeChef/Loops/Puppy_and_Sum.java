8be37be7b1aeeddefa89862192edf8ac94e146a0
Bikash Nath <splittergame91@gmail.com>
2022-11-22 12:34:26 +0530

    implementation

---
+++ _CodeChef/Loops/Puppy_and_Sum.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Puppy_and_Sum {
+
+    static Scanner scan = new Scanner(System.in);
+
+
+    public static void main(String[] args) {
+        int t = scan.nextInt();
+        
+        while(t-->0){
+
+            int d = scan.nextInt();
+            int n = scan.nextInt();
+            int sum=0;
+
+            for (int i=1; i<=d;i++){
+
+                sum = (n*(n+1))/2;
+                n = sum;
+            }
+
+            System.out.println(sum);
+        }
+    }
+}
