df44e63b43a12cee9c4714909a8560c21b7e7a58
Bikash Nath <splittergame91@gmail.com>
2022-11-23 16:53:50 +0530

    Inheritance -1

---
+++ OOPS/SingleInheritance.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+class SingleInheritance{
+
+    public static void main(String[] args) {
+        
+        std_id pn = new std_id();
+
+
+        pn.input();
+        pn.disp();
+    }
+
+}
+
+
+class students{
+    static Scanner s = new Scanner(System.in);
+    int roll, marks;
+    String name;
+    protected int id;
+
+    void input(){
+
+        System.out.println("What is you'r name? ");
+        name = s.next();
+        System.out.println("roll no: ");
+        roll = s.nextInt();
+        System.out.println("Enter marks: ");
+        marks = s.nextInt();
+        
+    }
+}
+
+
+class std_id extends students{
+
+    void disp(){
+        System.out.println(marks + " marks obtained by "+name+ " & roll no is "+roll);
+    }
+}
