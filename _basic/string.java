69a27f182d3caffa27d3dd3688fba9203a5d35b5
Bikash Nath <splittergame91@gmail.com>
2022-11-29 21:51:44 +0530

    string functions

---
+++ _basic/string.java
@@ -0,0 +1,43 @@
+public class string {
+    
+    public static void main(String[] args) {
+        
+        
+
+        String str = "hello world";
+
+        String str1 = "hello";
+
+        // if (str == str1){
+        //     System.out.println("equal");
+        // }
+        // else
+        // System.out.println("not equal");
+
+
+
+        // System.out.println(str);
+
+        String str2 = new String(" hello world");
+
+        // System.out.println(str2);
+
+        // System.out.println(str.length());
+        // System.out.println(str.charAt(4));
+
+        // System.out.println(str.substring(0, 5));
+
+        // System.out.println(str.indexOf("w"));
+
+
+        String new_str = str.concat(str2);
+        
+        System.out.println(new_str);
+
+        // if (str.equals(str2)){
+        //     System.out.println("both are equal");
+        // }
+        // else
+        // System.out.println("not equal");
+    }
+}
