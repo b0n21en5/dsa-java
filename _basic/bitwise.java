b99da124a30ef4165ac80c14b5c0f632fc3ef676
Bikash Nath <splittergame91@gmail.com>
2022-12-08 21:44:47 +0530

    bitwise operator

---
+++ _basic/bitwise.java
@@ -0,0 +1,14 @@
+class bitwise{
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 4;
+
+
+        System.out.println("bitwise and result is: " + (a & b));  // bitwise and ( & )
+
+
+        System.out.println("bitwise or result is: " + (a | b));    // bitwise or  ( | )
+
+        System.out.println("bitwise xor result is: " + (a ^ b));   //bitwise XOR ( ^ )
+    }
+}
